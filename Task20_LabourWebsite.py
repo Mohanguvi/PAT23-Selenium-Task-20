3765ecc294d26b6cdb752d3d1e195e153003442f
Mohan raj <147843991+Mohanguvi@users.noreply.github.com>
2024-02-27 23:16:06 +0530

    Task20_LabourWebsite.py

--- Task20_LabourWebsite.py
+++ Task20_LabourWebsite.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import os
 import requests
 from time import sleep
 
